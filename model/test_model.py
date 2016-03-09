82f8d9acc6c2b246e791bb32792fc55703f3e436
Thom Young <tom@hensonyoung.net>
2016-03-10 11:24:22 +1300

    Fixed tests for latest api

--- model/test_model.py
+++ model/test_model.py
@@ -23,33 +23,14 @@ class TestModel(unittest.TestCase):
         factor_values = [0, 1, 0, 1, 1, 0, 1] # factor_defns[for selection] = 1.0, meaning no selection! So zero fitness entities persist...
         factors = [defn[value] for defn, value in zip(factor_defns, factor_values)]
         #print(factors)
-        (initial, final) = model.run(factors, population=init_population(5000, low_start=True), generations=250, population_limit=10, changing_environment=True)
-        self.assertEqual(5000, final.pop)
+        results = model.run(factors, population=init_population(5000, low_start=True), generations=250, population_limit=10, environment_change_frequency=1)
+        self.assertEqual(5000, results[-1]['pop'])
 
     def test_selection(self):
         #return [x for x in population if get_random_boolean(x.fitness if factors[1] == 0 else factors[1])]
         population = [model.Element(0.0,0.0) for i in range(0,1000)]
         self.assertEqual(0,len(model.selection([0,0], population))) # use element fitness
         self.assertEqual(len(population), len(model.selection([0,1.0], population))) # use provided fitness = 1.0 so should be no selection
-
-    def test_lambda_distributions(self):
-        gauss = factor_defns[4][0]
-        uniform = factor_defns[4][1]
-        # both restricted to range [0,1]
-        # gauss is described by mean=source, sd=1-correlation, then clipped to [0,1]
-        # uniform to range [source-(1-correlation), source+(1+correlation)], then clipped to [0,1]
-        gauss_values = [gauss(0.5,0.7) for i in range(0,1000)]  # mean in middle of range, so clipping won't introduce bias
-        uniform_values = [uniform(0.5,0.7) for i in range(0,1000)]
-
-        self.assertAlmostEqual(statistics.mean(gauss_values), statistics.mean(uniform_values), places=1)
-        self.assertNotAlmostEqual(statistics.stdev(gauss_values), statistics.stdev(uniform_values), places=1)
-
-        gauss1_values = [gauss(0.5,0.4) for i in range(0,1000)]
-        gauss2_values = [gauss(0.3,0.4) for i in range(0,1000)]
-        self.assertTrue(statistics.mean(gauss1_values) > statistics.mean(gauss2_values))
-
-        gauss1_values = [gauss(0.2,1.0) for i in range(0,100)] # 2nd parameter is not sd, but 1.0-sd
-        self.assertAlmostEqual(0.0, statistics.stdev(gauss1_values))
 
     def test_reproduction(self):
 

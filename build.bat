aa1d5722be6cc66988c01c30f0c77177af9aa3ea
Thomas Young <tom@hensonyoung.net>
2016-04-18 09:47:27 +1200

    Build tidies up temp tex files

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
-"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('body.Rnw')"
+Rscript.exe -e "library(knitr); knit('mythesis.Rnw')"
+Rscript.exe -e "library(knitr); knit('body.Rnw')"
 
 latexmk -pdf mythesis
-REM makeglossaries mythesis
-REM latexmk -pdf mythesis
 latexmk -c mythesis
+
+del tmp-pdfcrop*.tex

6561320f0f48bcb4ee073db900d20bc3711434ad
Thom Young <tom@hensonyoung.net>
2016-04-19 17:37:03 +1200

    Continued tidy up of previous work and context sections

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-Rscript.exe -e "library(knitr); knit('mythesis.Rnw')"
-Rscript.exe -e "library(knitr); knit('part2.Rnw')"
-Rscript.exe -e "library(knitr); knit('part3.Rnw')"
+"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
+"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part2.Rnw')"
+"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part3.Rnw')"
 
 latexmk -pdf mythesis
 latexmk -c mythesis

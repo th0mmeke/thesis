4eed29b67a8db792e64fd89602a2f1c840d250f8
Thom Young <tom@hensonyoung.net>
2016-05-31 18:52:18 +1200

    Tidy ups

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part3.Rnw')"
 
 latexmk -pdf mythesis
-latexmk -c mythesis
+REM latexmk -c mythesis
 
 REM del part*.tex mythesis.tex tmp*.tex

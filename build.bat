3bc6fe458aeabe86b0edd8b40bc7144fdf79e67c
Thom Young <tom@hensonyoung.net>
2016-06-28 11:05:18 +1200

    Changed nameref to namecref

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part2.Rnw')"
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part3.Rnw')"
 
-latexmk -pdf mythesis
+REM latexmk -pdf mythesis
 REM latexmk -c mythesis
 
 REM del part*.tex mythesis.tex tmp*.tex

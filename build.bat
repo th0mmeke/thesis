dec7513b52ced4543329021e783ba44689c972e8
Thom Young <tom@hensonyoung.net>
2016-06-28 13:50:09 +1200

    Build that doesn't rebuild the bbl file (broken latex tools)

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part2.Rnw')"
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part3.Rnw')"
+pdflatex mythesis
+pdflatex mythesis
 
 REM latexmk -pdf mythesis
 REM latexmk -c mythesis

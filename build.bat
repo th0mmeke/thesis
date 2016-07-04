537f7d8b736b3bfa04eeb4ea49be4f18e5dba1f3
Thom Young <tom@hensonyoung.net>
2016-07-04 17:28:17 +1200

    Tidies

--- build.bat
+++ build.bat
@@ -1,10 +1,8 @@
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part2.Rnw')"
 "C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part3.Rnw')"
-pdflatex mythesis
-pdflatex mythesis
 
-REM latexmk -pdf mythesis
+latexmk -pdf mythesis
 REM latexmk -c mythesis
 
 REM del part*.tex mythesis.tex tmp*.tex

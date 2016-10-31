2abeabb34fa38f67188bb8a934841ef9236908ee
th0mmeke <tom@hensonyoung.net>
2016-10-31 17:17:00 +1300

    Updated for new R version

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
-"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part2.Rnw')"
-"C:\Program Files\R\R-3.2.4revised/bin/Rscript.exe" -e "library(knitr); knit('part3.Rnw')"
+"C:\Program Files\R\R-3.3.1/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
+"C:\Program Files\R\R-3.3.1/bin/Rscript.exe" -e "library(knitr); knit('part2.Rnw')"
+"C:\Program Files\R\R-3.3.1/bin/Rscript.exe" -e "library(knitr); knit('part3.Rnw')"
 
 latexmk -pdf mythesis
 REM latexmk -c mythesis

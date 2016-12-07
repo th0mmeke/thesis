0d3f89a333625686af1aa63b04d7337457c7c5ab
th0mmeke <tom@hensonyoung.net>
2016-12-07 17:36:15 +1300

    C-grade changes suggested by reviewers

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('toyworld.Rnw')"
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('model.Rnw')"
-"C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('toyworld2.Rnw')"
 
 latexmk -pdf mythesis

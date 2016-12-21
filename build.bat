3a56a375e5ba525f0eefbd208bdee7aa473b7299
th0mmeke <thomas.young@canterbury.ac.nz>
2016-12-22 11:13:04 +1300

    More updates to Toyworld2 material, and general structure

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('toyworld.Rnw')"
+"C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('toyworld2.Rnw')"
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('model.Rnw')"
 
 latexmk -pdf mythesis

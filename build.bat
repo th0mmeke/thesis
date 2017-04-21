d74db43b6c84ece54ea66c4aa7dbb831560860ba
Thomas Young <thomas.young@canterbury.ac.nz>
2017-04-21 14:51:57 +1200

    Split files

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('mythesis.Rnw')"
-"C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('toyworld.Rnw')"
-"C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('toyworld2.Rnw')"
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('model.Rnw')"
+"C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('toyworld.Rnw')"
+"C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('multiplication.Rnw')"
+"C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('variability.Rnw')"
 "C:\Program Files\R\R-3.3.2/bin/Rscript.exe" -e "library(knitr); knit('appendices.Rnw')"
 
 latexmk -pdf mythesis
